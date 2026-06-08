0c63fad1eb9f1e6ae8e832cb858c173ca7bbb9fb
hahahai94 <dmango469@gmail.com>
2026-06-08 19:50:01 +1000

    Added new models in admin-panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from tasks.models import TaskList, Task
 
-# Register your models here.
+
+@admin.register(TaskList)
+class TaskListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'task_count', 'done_count', 'progress_bar', 'created_at')
+    list_filter = ('created_at', 'owner')
+    search_fields = ('name', 'description', 'owner__username')
+    ordering = ('-created_at',)
+
+    def progress_bar(self, obj):
+        total = obj.task_count()
+        done = obj.done_count()
+        ratio = (done / total * 100) if total else 0
+        color = 'success' if ratio == 100 else ('warning' if ratio > 0 else 'secondary')
+        return format_html(
+            '<div class="progress" style="height: 16px; width: 120px;">'
+            '<div class="progress-bar bg-{}" style="width: {}%;">{}/{}</div>'
+            '</div>',
+            color, ratio, done, total
+        )
+
+    progress_bar.short_description = "Прогресс"
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task_list', 'created_by', 'assigned_to', 'is_done', 'created_at', 'completed_at')
+    list_filter = ('is_done', 'created_at', 'completed_at')
+    search_fields = ('title', 'description', 'created_by__username', 'assigned_to__username')
+    ordering = ('-created_at',)
+    list_select_related = ('task_list', 'created_by', 'assigned_to')
--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.html import format_html
-from django.utils.timezone import now
-from django.contrib.sessions.models import Session
 from django.http import HttpResponse
-from django.contrib import messages
-from users.models import BannedIP
-import csv, os
+from users.models import BannedIP, AdminLog, Notification
+import csv
 
 User = get_user_model()
 
@@ -25,17 +22,19 @@ def export_to_csv(modeladmin, request, queryset, filename, fields, headers):
 
 @admin.register(User)
 class UltimateUserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'is_active', 'is_staff', 'last_login', 'avatar_preview')
+    list_display = ('username', 'nick', 'email', 'is_active', 'is_banned', 'is_staff', 'is_superuser', 'last_seen', 'avatar_preview')
     list_filter = ('is_active', 'is_staff', 'is_superuser', 'groups', 'last_login')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'nick', 'email', 'first_name', 'last_name')
     ordering = ('-last_login',)
-    readonly_fields = ('date_joined', 'last_login', 'password')
+    readonly_fields = ('date_joined', 'last_login', 'last_seen', 'password', 'banned_at')
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Личные данные', {'fields': ('first_name', 'last_name', 'email', 'avatar')}),
+        ('Профиль', {'fields': ('nick', 'avatar', 'bio')}),
+        ('Личные данные', {'fields': ('first_name', 'last_name', 'email')}),
         ('Статус', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Даты', {'fields': ('last_login', 'date_joined')}),
+        ('Бан', {'fields': ('ban_reason', 'banned_at', 'banned_by')}),
+        ('Активность', {'fields': ('last_login', 'last_seen', 'date_joined')}),
     )
 
     def avatar_preview(self, obj):
@@ -46,6 +45,12 @@ class UltimateUserAdmin(BaseUserAdmin):
         return "-"
 
     avatar_preview.short_description = "Фото"
+
+    def is_banned(self, obj):
+        return obj.banned_at is not None
+
+    is_banned.boolean = True
+    is_banned.short_description = "Забанен"
 
 
 @admin.register(BannedIP)
@@ -59,3 +64,26 @@ class BannedIPAdmin(admin.ModelAdmin):
         if not obj.pk:
             obj.banned_by = request.user
         super().save_model(request, obj, form, change)
+
+
+@admin.register(AdminLog)
+class AdminLogAdmin(admin.ModelAdmin):
+    list_display = ('admin', 'action', 'target_user', 'timestamp', 'ip_address')
+    list_filter = ('action', 'timestamp', 'admin')
+    search_fields = ('admin__username', 'target_user__username', 'description')
+    readonly_fields = ('admin', 'action', 'target_user', 'description', 'timestamp', 'ip_address')
+    ordering = ('-timestamp',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'notification_type', 'title', 'is_read', 'created_at')
+    list_filter = ('notification_type', 'is_read', 'created_at')
+    search_fields = ('user__username', 'title', 'message')
+    ordering = ('-created_at',)
